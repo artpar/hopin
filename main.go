486bd6fec4ec6dd5dab85a8a274517e5bae44933
Parth Mudgal <artpar@gmail.com>
2014-10-02 21:57:45 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gorilla/mux"
+import "net/http"
+
+func main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/", HomeHandler)
+    http.Handle("/", r)
+	http.ListenAndServe(":30824", nil)
+}
+
+
+func HomeHandler(response http.ResponseWriter , request *http.Request ) {
+	response.Write([]byte("Hello"))
+}
